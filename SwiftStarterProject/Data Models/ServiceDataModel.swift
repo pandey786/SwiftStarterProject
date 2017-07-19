a28ac00d1350f1e2ff9eb738a34f2e892811a15c
Durgesh Pandey <durgesh.pandey2@cognizant.com>
2017-07-19 18:29:49 +0530

    first commit

---
+++ SwiftStarterProject/Data Models/ServiceDataModel.swift
@@ -0,0 +1,9 @@
+//
+//  ServiceDataModel.swift
+//  SwiftStarterProject
+//
+//  Created by Pandey on 18/07/17.
+//  Copyright © 2017 Pandey. All rights reserved.
+//
+
+import Foundation
---
+++ SwiftStarterProject/View Controllers/SongsListTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  SongsListTVCell.swift
+//  SwiftStarterProject
+//
+//  Created by Pandey on 18/07/17.
+//  Copyright © 2017 Pandey. All rights reserved.
+//
+
+import UIKit
+
+class SongsListTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
